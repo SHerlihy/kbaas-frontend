a3ef8032490ed108929bedc72bd386433031fc40
SHerlihy <steven_herlihy@yahoo.com>
2025-12-23 07:04:28 +0000

    draft query controls

---
+++ src/features/queryStory/QueryStoryControl.tsx
@@ -0,0 +1,51 @@
+import { Card, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { PostMarkStory } from './QueryStoryModel'
+import { catchError } from '@/lib/async'
+
+interface IQueryStoryControl {
+    postQuery: PostMarkStory
+    abortQuery: (reason?: any) => void
+}
+
+class QueryStoryControl implements IQueryStoryControl {
+    postUrl = ""
+    controller = new AbortController()
+
+    getParam: () => string;
+
+    constructor(getParam: () => string) {
+        this.getParam = getParam
+    }
+
+    async postQuery(story: string): Promise<[undefined, string] | [Error]> {
+        const param = this.getParam()
+        const params = new URLSearchParams();
+        params.append("key", param)
+
+        const response = await fetch(`${this.postUrl}?${params}`, {
+            method: "POST",
+            mode: "cors",
+            signal: this.controller.signal,
+            body: story
+        })
+
+        if (!response.ok) {
+            return [new Error(`Query story status: ${response.status}`)]
+        }
+
+        const [error, marked] = await catchError(response.text())
+
+        if (error) {
+            return [error]
+        }
+
+        return [undefined, marked]
+    }
+
+    abortQuery(reason?: any) {
+        this.controller.abort(reason)
+    }
+}
+
+export default QueryStoryControl
